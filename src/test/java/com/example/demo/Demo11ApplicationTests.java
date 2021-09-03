4a8e7458f71e16a5a35deba38a644a5855e9d244
Mohamed Ali GHOUMA <uid1354@focus.com.tn>
2021-09-03 16:08:28 +0100

    test commit

--- src/test/java/com/example/demo/Demo11ApplicationTests.java
+++ src/test/java/com/example/demo/Demo11ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 class Demo11ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 assertThat("bar").isEqualTo("bar");	
 	}
 
 }
